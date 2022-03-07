d9f4c9b8af18dfd51cecca795215f4356b12800b
leonistor <leo.nistor@gmail.com>
2022-03-07 14:56:34 +0200

    web: list partials for projects

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,10 +1,17 @@
 import { resolve } from 'path'
+import { readdirSync } from 'fs'
 import handlebars from 'vite-plugin-handlebars'
+
+let partials = resolve(__dirname, 'partials')
+let project_files = readdirSync(partials).filter((file) => file.startsWith("proj"))
 
 export default {
     plugins: [
         handlebars({
-            partialDirectory: resolve(__dirname, 'partials'),
+            partialDirectory: partials,
+            context: {
+                projects: project_files
+            }
         }),
     ],
 }
