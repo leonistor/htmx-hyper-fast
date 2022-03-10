d83418f68782e0fb95b1c4ca5dced640767e46ff
leonistor <leo.nistor@gmail.com>
2022-03-10 14:01:08 +0200

    web: display credits in index

--- web/vite.config.js
+++ web/vite.config.js
@@ -5,15 +5,48 @@ import handlebars from 'vite-plugin-handlebars'
 let partials = resolve(__dirname, 'partials')
 let project_files = readdirSync(__dirname)
     .filter((file) => file.startsWith("proj") && file.endsWith("html"))
-
-// TODO: credits
+let credits = [
+    {
+        url: "https://bigsky.software/",
+        info: "HTMX and Hyperscript"
+    },
+    {
+        url: "https://50projects50days.com/",
+        info: "Traversy Media (50 projects)"
+    },
+    {
+        url: "https://fastapi.tiangolo.com/",
+        info: "FastAPI"
+    },
+    {
+        url: "https://pipenv.pypa.io/en/latest/",
+        info: "Pipenv"
+    },
+    {
+        url: "https://glyphs.fyi/",
+        info: "The Mightiest Icons"
+    },
+    {
+        url: "https://grayshift.io/",
+        info: "Grayshift CSS"
+    },
+    {
+        url: "https://vitejs.dev/",
+        info: "Vite Build Tool"
+    },
+    {
+        url: "https://caddyserver.com/",
+        info: "Caddy 2"
+    },
+]
 
 export default {
     plugins: [
         handlebars({
             partialDirectory: partials,
             context: {
-                projects: project_files
+                projects: project_files,
+                credits,
             }
         }),
     ],
